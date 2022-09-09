2cf22298e92a90aab9bcbd51c4d4e03c5c23abc1
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-09-10 03:10:37 +0900

    test: Create order test

---
+++ src/main/java/org.example/Customer.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Customer {
+    public void order(String menuName, Menu menu, Cooking cooking) {
+        Menuitem menuitem = menu.choose(menuName);
+        Cook cook = cooking.makeCook(menuitem);
+    }
+}
