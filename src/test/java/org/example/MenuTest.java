1b8d938c9f408cfebd5be545a7622063d0cabd9e
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-09-10 02:56:53 +0900

    test: Create choose test; 메뉴판에서 없는 메뉴를 선택할 시 예외를 반환

--- src/test/java/org/example/MenuTest.java
+++ src/test/java/org/example/MenuTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class MenuTest {
 
@@ -16,5 +17,14 @@ public class MenuTest {
 
         Menuitem menuitem = menu.choose("돈까스");
         assertThat(menuitem).isEqualTo(new Menuitem("돈까스", 5000));
+    }
+    @DisplayName("메뉴판에서 없는 메뉴를 선택할 시 예외를 반환한다")
+    @Test
+    void chooseTest2() {
+        Menu menu = new Menu(List.of(new Menuitem("돈까스", 5000), new Menuitem("냉면", 7000)));
+
+        assertThatCode(() -> menu.choose("통닭"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잘못된 메뉴 이름입니다");
     }
 }
