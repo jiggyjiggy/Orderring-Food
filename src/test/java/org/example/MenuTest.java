1745f4a09be247bfcc6442b2527f5e34c1599f6a
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-09-10 02:36:22 +0900

    chore: Fix display name of choose test

--- src/test/java/org/example/MenuTest.java
+++ src/test/java/org/example/MenuTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MenuTest {
 
-    @DisplayName("메뉴 이름에 해당하는 메뉴를 반환한다")
+    @DisplayName("메뉴판에서 메뉴 이름에 해당하는 메뉴를 반환한다")
     @Test
     void chooseTest() {
         Menu menu = new Menu(List.of(new Menuitem("돈까스", 5000), new Menuitem("냉면", 7000)));
