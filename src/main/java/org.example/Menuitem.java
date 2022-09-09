7ccdea6eae9227d89c680d6733a8bfc2ae1f7831
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-09-10 01:23:47 +0900

    test: Create menu item test

---
+++ src/main/java/org.example/Menuitem.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Menuitem {
+    private final String name;
+    private final int price;
+
+    public Menuitem(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ src/test/java/org/example/MenuitemTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class MenuitemTest {
+
+    @DisplayName("메뉴를 생성한다")
+    @Test
+    void createTest() {
+        assertThatCode(() -> new Menuitem("만두", 5000))
+                .doesNotThrowAnyException();
+    }
+}
